f5acfd15112376f9d887f5f5585f00a6116f7636
Abhishek <abhi1234456@gmail.com>
2018-04-03 00:17:00 +0530

    Major Changes includin User login System

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired , Email, Length
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 # from flask_mail import Mail, Message ## not in use anymore 
 from datetime import datetime
 
 app = Flask( __name__ )
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/abhishek/Videos/flaskWorks/Blog/final.db'
-
+app.config['SECRET_KEY'] = "Donottellanyone"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///home/abhishek/Videos/flaskWorks/Blog/database.db'
+Bootstrap(app)
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 # mail = Mail(app)
 
 class Post(db.Model):
@@ -26,6 +38,57 @@ class Comment(db.Model):
 	message = db.Column(db.Text)
 	date = db.Column(db.DateTime)
 
+class User(UserMixin, db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(15), unique=True)
+	email = db.Column(db.String(100), unique=True)
+	password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class LoginForm(FlaskForm):
+	username = StringField('username', validators=[InputRequired(), Length(min=4 , max=15)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+	remember = BooleanField('remember me')
+
+
+class RegisterForm(FlaskForm):
+	email = StringField('email', validators=[InputRequired(), Email(message='Invalid Email'), Length(max=100)])
+	username = StringField('username', validators=[InputRequired(), Length(min=4 , max=15)])
+	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])	
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user:
+			if check_password_hash(user.password, form.password.data):
+				login_user(user, remember=form.remember.data)
+				return redirect(url_for('admin'))
+
+		return "Invalid username or password"
+
+	return render_template('login.html', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	form = RegisterForm()
+
+	if form.validate_on_submit():
+		hashed_pass = generate_password_hash(form.password.data, method='sha256')
+		new_user = User(username=form.username.data, email=form.email.data, password=hashed_pass)
+		db.session.add(new_user)
+		db.session.commit()
+
+		return "New user registered !"
+
+	return render_template('signup.html', form=form)
+
 
 @app.route('/')
 def index():
@@ -44,10 +107,12 @@ def post(post_id):
 	return render_template('post.html', post=post, date_posted=date_posted)
 
 @app.route('/add')
+@login_required
 def add():
 	return render_template('add.html')
 
 @app.route('/addpost', methods=['GET', 'POST'])
+@login_required
 def addpost():
 	title = request.form['title']
 	subtitle = request.form['subtitle']
@@ -78,6 +143,17 @@ def addFeed():
 	db.session.commit()
 
 	return redirect(url_for('index'))
+
+@app.route('/admin')
+@login_required
+def admin():
+	return render_template('admin.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
